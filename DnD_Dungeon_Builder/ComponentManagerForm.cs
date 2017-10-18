f70bdc5988ea1261d3c9ea016327a332310ec351
Marco <marcovman@hotmail.com>
2017-10-18 14:39:00 +0200

    added copy to methods

--- DnD_Dungeon_Builder/ComponentManagerForm.cs
+++ DnD_Dungeon_Builder/ComponentManagerForm.cs
@@ -34,6 +34,96 @@ namespace DnD_Dungeon_Builder
             pbWestIsometric.SizeMode = PictureBoxSizeMode.StretchImage;
 
             updateInfo();
+
+            pbNorth2D.ContextMenu = createContextMenu(Position.North);
+            pbEast2D.ContextMenu = createContextMenu(Position.East);
+            pbSouth2D.ContextMenu = createContextMenu(Position.South);
+            pbWest2D.ContextMenu = createContextMenu(Position.West);
+
+        }
+
+        private ContextMenu createContextMenu(Position position)
+        {
+            ContextMenu cm = new ContextMenu();
+
+            if (position != Position.North)
+            {
+                MenuItem item = new MenuItem("Copy to North", new EventHandler(CopyToNorth_Click));
+                item.Name = position.ToString();
+                cm.MenuItems.Add(item);
+            }
+            if (position != Position.East)
+            {
+                MenuItem item = new MenuItem("Copy to East", new EventHandler(CopyToEast_Click));
+                item.Name = position.ToString();
+                cm.MenuItems.Add(item);
+            }
+            if (position != Position.South)
+            {
+                MenuItem item = new MenuItem("Copy to South", new EventHandler(CopyToSouth_Click));
+                item.Name = position.ToString();
+                cm.MenuItems.Add(item);
+            }
+            if (position != Position.West)
+            {
+                MenuItem item = new MenuItem("Copy to West", new EventHandler(CopyToWest_Click));
+                item.Name = position.ToString();
+                cm.MenuItems.Add(item);
+            }
+
+            return cm;
+        }
+
+        private void CopyToWest_Click(object sender, EventArgs e)
+        {
+            if (sender is MenuItem)
+            {
+                MenuItem menuItem = sender as MenuItem;
+                Position position = Position.NotSet;
+                if (Enum.TryParse(menuItem.Name, out position))
+                {
+                    rotateBitmap(position, Position.West);
+                }
+            }
+        }
+
+        private void CopyToSouth_Click(object sender, EventArgs e)
+        {
+            if (sender is MenuItem)
+            {
+                MenuItem menuItem = sender as MenuItem;
+                Position position = Position.NotSet;
+                if (Enum.TryParse(menuItem.Name, out position))
+                {
+                    rotateBitmap(position, Position.South);
+                }
+            }
+        }
+
+        private void CopyToEast_Click(object sender, EventArgs e)
+        {
+            if (sender is MenuItem)
+            {
+                MenuItem menuItem = sender as MenuItem;
+                Position position = Position.NotSet;
+                if (Enum.TryParse(menuItem.Name, out position))
+                {
+                    rotateBitmap(position, Position.East);
+                }
+            }
+        }
+
+        private void CopyToNorth_Click(object sender, EventArgs e)
+        {
+            if (sender is MenuItem)
+            {
+                MenuItem menuItem = sender as MenuItem;
+                Position position = Position.NotSet;
+                if (Enum.TryParse(menuItem.Name, out position))
+                {
+                    rotateBitmap(position, Position.North);
+                }
+            }
         }
 
         private void btnAddComponent_Click(object sender, EventArgs e)
@@ -212,6 +302,45 @@ namespace DnD_Dungeon_Builder
             isometric.RotateFlip(RotateFlipType.RotateNoneFlipX);
 
             selectedComponent.AddDrawing(new Drawing(twoD, isometric, Position.South));
+            updateInfo(selectedComponent);
+        }
+
+        private void rotateBitmap(Position basePosition, Position targetPosition)
+        {
+            int baseRotation = (int)basePosition;
+            int targetRotation = (int)targetPosition;
+
+            int rotation = targetRotation - baseRotation;
+            rotation = (rotation < 0) ? 360 + rotation : rotation;
+
+            RotateFlipType rotateFlipType = RotateFlipType.RotateNoneFlipNone;
+
+            switch (rotation)
+            {
+                case 0:
+                    rotateFlipType = RotateFlipType.RotateNoneFlipNone;
+                    break;
+                case 90:
+                    rotateFlipType = RotateFlipType.Rotate90FlipNone;
+                    break;
+                case 180:
+                    rotateFlipType = RotateFlipType.Rotate180FlipNone;
+                    break;
+                case 270:
+                    rotateFlipType = RotateFlipType.Rotate270FlipNone;
+                    break;
+                default:
+                    break;
+            }
+
+            Drawing drawingToCopy = selectedComponent.GetDrawing(basePosition);
+            Drawing targetOriginal = selectedComponent.GetDrawing(targetPosition);
+            Bitmap twoD = (Bitmap)drawingToCopy.TwoDView.Clone();
+            Bitmap isometric = (targetOriginal != null) ? targetOriginal.ThreeDView : null;
+
+            twoD.RotateFlip(rotateFlipType);
+
+            selectedComponent.AddDrawing(new Drawing(twoD, isometric, targetPosition));
             updateInfo(selectedComponent);
         }
     }
--- DnD_Dungeon_Builder/Drawing.cs
+++ DnD_Dungeon_Builder/Drawing.cs
@@ -4,11 +4,11 @@ namespace DnD_Dungeon_Builder
 {
     public enum Position
     {
-        North,
-        East,
-        South,
-        West,
-        NotSet
+        North = 0,
+        East = 90,
+        South = 180,
+        West = 270,
+        NotSet = -1
     }
 
     public class Drawing
