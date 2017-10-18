2373e4abcfcf6dad86b9a14176b3f2f7a1e622a2
Marco <marcovman@hotmail.com>
2017-10-18 13:48:03 +0200

    removed unused usings

--- DnD_Dungeon_Builder/BitmapBackup.cs
+++ DnD_Dungeon_Builder/BitmapBackup.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DnD_Dungeon_Builder
 {
-    class BitmapBackup
+    public class BitmapBackup
     {
         public PictureBox PictureBox { get; private set; }
         public Bitmap Bitmap { get; private set; }
