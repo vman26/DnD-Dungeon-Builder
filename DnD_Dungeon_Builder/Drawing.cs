8b7bc00aaed3da660b4b1777a07e062ecac649d8
Marco <marcovman@hotmail.com>
2017-10-19 10:13:38 +0200

    moved position enum to enum.cs

--- DnD_Dungeon_Builder/Drawing.cs
+++ DnD_Dungeon_Builder/Drawing.cs
@@ -2,15 +2,6 @@
 
 namespace DnD_Dungeon_Builder
 {
-    public enum Position
-    {
-        North = 0,
-        East = 90,
-        South = 180,
-        West = 270,
-        NotSet = -1
-    }
-
     public class Drawing
     {
         public Bitmap TwoDView { get; private set; }
--- DnD_Dungeon_Builder/Enum.cs
+++ DnD_Dungeon_Builder/Enum.cs
@@ -11,4 +11,13 @@ namespace DnD_Dungeon_Builder
         TwoDimensional = 0,
         Isometric = 1,
     }
+    
+    public enum Position
+    {
+        North = 0,
+        East = 90,
+        South = 180,
+        West = 270,
+        NotSet = -1
+    }
 }
