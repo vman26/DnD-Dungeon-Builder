7b4695f714ec75f114b8f40595ca663e3ab748e6
Marco <marcovman@hotmail.com>
2017-10-23 16:26:23 +0200

    non transparent version

--- DnD_Dungeon_Builder/Draw.cs
+++ DnD_Dungeon_Builder/Draw.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace DnD_Dungeon_Builder
 {
@@ -148,6 +149,29 @@ namespace DnD_Dungeon_Builder
             Graphics g = Graphics.FromImage(bitmap);
             g.Clear(Color.Transparent);
             g.Dispose();
+        }
+
+        public static GraphicsPath Transparent(Image im)
+        {
+            int x;
+            int y;
+            Bitmap bmp = new Bitmap(im);
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            Color mask = bmp.GetPixel(0, 0);
+
+            for (x = 0; x <= bmp.Width - 1; x++)
+            {
+                for (y = 0; y <= bmp.Height - 1; y++)
+                {
+                    if (!bmp.GetPixel(x, y).Equals(mask))
+                    {
+                        gp.AddRectangle(new Rectangle(x, y, 1, 1));
+                    }
+                }
+            }
+            bmp.Dispose();
+            return gp;
+
         }
     }
 }
